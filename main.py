4d5a6dd455d69fbf7e03297cb8ebb1972dcd4c91
ManjunathAnde <manjunathande7@gmail.com>
2026-08-13 13:31:43 +0530

    Product lookup

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ def greet():
     return "Welcome to MJ APP"
 
 products = [  #Instantiating the classes
-    Products(1, "CRICKET BAT", "ADIDAS", 2000, "5"),
-    Products(2, "OPPO PHONE", "256 GB", 55000, "12")
+    Products(id=1, name="Phone", description="A smartphone", price=699.99, quantity=50),
+    Products(id=2, name="Laptop", description="A powerful laptop", price=999.99, quantity=30),
+    Products(id=3, name="Pen", description="A blue ink pen", price=1.99, quantity=100),
+    Products(id=4, name="Table", description="A wooden table", price=199.99, quantity=20)
 ]
 
 @app.get("/products") #usinf GET method to display information when the user routes to 'skills' in the web app. 
 def get_products():
     return products
+
+sno:int=1
+@app.get("/products/{sno}") #A dynamic URL to fetch products by ID
+def get_single_product(sno:int): 
+    for product in products: #Looking up the product roster to find matching ID
+        if product.id == sno:
+            return{'message': 'Hurray! Product found',
+                   'prod_details': product
+                   } #If found, return the instance.
--- models.py
+++ models.py
@@ -6,12 +6,4 @@ class Products (BaseModel):
     description:str
     price:float
     quantity:int
-
-    #Constructor 
-    def __init__(self, id: int, name: str, description: str, price: float, quantity: int):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
+#Pydantic takes care of the constructor
