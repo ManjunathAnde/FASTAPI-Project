45a81ca9000ed6ad572269167949b55eca265ef2
ManjunathAnde <manjunathande7@gmail.com>
2026-08-13 12:09:15 +0530

    Products module

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from models import Products
 app = FastAPI() #creating an instance of FASTAPI
 
-@app.get("/") 
+@app.get("/") # using GET HTTP method to see information in homepage
 def greet():
     return "Welcome to MJ APP"
 
-@app.get("/skills")
-def skills():
-    return {"skills": ["Python", "FastAPI", "AWS"]}
+products = [
+    Products(1, "CRICKET BAT", "ADIDAS", "2000", "5"),
+    Products(2, "OPPO PHONE", "256 GB", "55000", "12")
+]
+
+@app.get("/products") #usinf GET method to display information when the user routes to 'skills' in the web app. 
+def products():
+    return 
