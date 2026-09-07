550e46e9cc8caaa50f31ec60e066e7a087161e06
ManjunathAnde <manjunathande7@gmail.com>
2026-09-07 14:27:50 -0400

    MySQL declarative_base config

--- database_models.py
+++ database_models.py
@@ -3,9 +3,11 @@ from sqlalchemy import Column, Integer, String, Float
 
 dec_base= declarative_base()
 
-class Products (dec_base): 
-    id=Column(Integer, primary_key=True, index=True)
-    name=Column(String)
-    description=Column(String)
-    price=Column(String)
-    quantity=Column(String)
+class Products(dec_base): 
+    __tablename__ = "fastapi_main"  
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), index=True)
+    description = Column(String(500))
+    price = Column(String(20))
+    quantity = Column(Integer)
