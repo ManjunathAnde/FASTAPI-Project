6bb368ac632fbe88e2f23651e1b11d9fcc653aad
ManjunathAnde <manjunathande7@gmail.com>
2026-09-06 23:03:09 -0400

    Stop tracking pycache folder

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ print(products)
 
 @app.put("/products")
 def update_product(id:int,product:Products):
-    for i in range(len(Products)):
-        if Products[i].id == id:
-            Products[i] == product
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
             return "Product update successful"
     return "Product not found"
